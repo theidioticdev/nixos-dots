0c55265339c46f26f21e06b60314111a5f64d680
theidioticdev <supersaiyanmario2009@gmail.cin>
2026-04-26 23:14:46 +0300

    update, add oxwm config

--- flake.nix
+++ flake.nix
@@ -7,9 +7,12 @@
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }: 
   let
     system = "x86_64-linux";
-    unstable = import nixpkgs-unstable { inherit system; config.allowunfree = true; };
+    unstable = import nixpkgs-unstable { 
+      inherit system; 
+      config.allowUnfree = true; 
+    };
   in {
-    nixosconfigurations.mostafa = nixpkgs.lib.nixossystem {
+    nixosConfigurations.mostafa = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
         ./configuration.nix
